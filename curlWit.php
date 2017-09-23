f50630baad633facff7932c1e41af00eab4cf947
Sigit Prabowo <sgt@prbw.net>
2017-09-23 11:55:19 +0700

    test

--- curlWit.php
+++ curlWit.php
@@ -5,7 +5,7 @@ $context = stream_context_create(array(
         'header'  => "Authorization: Bearer GARBPU36ZBU73SYODXAKYQZBDMVWFFBT"
     )
 ));
-$url = "https://api.wit.ai/message?v=".date('d-m-Y')."&q=".$_GET['id'];
+$url = "https://api.wit.ai/message?v=".date('d-m-Y')."&q=".rawurlencode($_GET['id']);
 $data = file_get_contents($url, false, $context);
 echo $data;
 ?>
